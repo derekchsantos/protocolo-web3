0b3e5a82b2ff1b6ac25ebc07c3e23eb2c43bab7e
Derek Christopher Santos <48655103+derekchsantos@users.noreply.github.com>
2026-06-02 17:19:09 -0300

    Update Governance.sol

--- contracts/Governance.sol
+++ contracts/Governance.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
+//@author Derek Christopher
+
 contract Governance {
     struct Proposal { string desc; uint256 votes; bool exec; }
     Proposal[] public proposals;
