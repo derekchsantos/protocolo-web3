661f7aa2c9f414dceb04cbc609550f363084261b
Derek Christopher Santos <48655103+derekchsantos@users.noreply.github.com>
2026-06-02 17:19:28 -0300

    Update NFT.sol

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
+
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+//@author Derek Christopher
 
 contract NFT is ERC721 {
     constructor() ERC721("Meu NFT", "MNFT") {}
