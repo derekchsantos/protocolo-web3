b0e22809e36aa887e2625c23e2e0b69b1d03f132
Derek Christopher Santos <48655103+derekchsantos@users.noreply.github.com>
2026-06-06 17:14:35 -0300

    NFT contract

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -3,8 +3,6 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-//@author Derek Christopher
-
 contract NFT is ERC721 {
     constructor() ERC721("Meu NFT", "MNFT") {}
 
